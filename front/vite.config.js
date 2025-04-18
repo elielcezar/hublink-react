5bee286b172efe5f01be8d629db92e8e20ee2be4
Eliel Cezar <elielcezar@gmail.com>
2025-04-18 12:18:43 -0300

    ajuste de ambiente

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,23 +1,31 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/uploads': {
-        target: 'http://localhost:3002',
-        changeOrigin: true
-      },
-      '/api': {
-        target: 'http://localhost:3002',
-        changeOrigin: true
+export default defineConfig(({ mode }) => {
+  // Carrega env files baseado no modo (.env.production ou .env.development)
+  // eslint-disable-next-line no-undef
+  loadEnv(mode, process.cwd(), '')
+  
+  return {
+    plugins: [react()],
+    server: {
+      port: 3000,
+      host: true,
+      proxy: {
+        '/uploads': {
+          target: 'http://localhost:3002',
+          changeOrigin: true
+        },
+        '/api': {
+          target: 'http://localhost:3002',
+          changeOrigin: true
+        }
       }
+    },
+    build: {
+      // Garantir que os caminhos relativos sejam preservados
+      assetsInlineLimit: 0
     }
-  },
-  build: {
-    // Garantir que os caminhos relativos sejam preservados
-    assetsInlineLimit: 0
   }
 })
